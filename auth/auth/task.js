384aefc3c1ff2735581c66917e879d3ff80be130
nadezhda-makarova <88685641+nadezhda-makarova@users.noreply.github.com>
2022-01-20 12:40:22 +0300

    Add files via upload

---
+++ auth/auth/task.js
@@ -0,0 +1,36 @@
+btn = document.getElementById('signin__btn');
+signin = document.getElementById('signin');
+signedForm = document.getElementById('signin__form');
+modal = document.getElementById('welcome');
+id_user = document.getElementById('user_id');
+form = {};
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  form = new FormData(signedForm);
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", 'https://netology-slow-rest.herokuapp.com/auth.php');
+  xhr.responseType = 'json';
+  xhr.send(form);
+
+  xhr.onload = () => {
+    const res = xhr.response;
+    if (res.success) {
+      localStorage.setItem('user_id', res.user_id);
+      showModal();
+    } else {
+      alert('Неверный логин/пароль');
+    };
+  };
+});
+
+const showModal = () => {
+  const isAuth = localStorage.getItem('user_id');
+  if (isAuth) {
+    signin.remove();
+    id_user.innerText = isAuth;
+    modal.classList.add('welcome_active');
+  };
+};
+
+showModal();
